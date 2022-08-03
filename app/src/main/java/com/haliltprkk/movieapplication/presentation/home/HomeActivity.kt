cd8974de227b46c1f6e31ef466d8c3541c1245d1
haliltprkk <haliltprkk@gmail.com>
2022-08-03 20:54:08 +0300

    Add basic activities

---
+++ app/src/main/java/com/haliltprkk/movieapplication/presentation/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.haliltprkk.movieapplication.presentation.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.haliltprkk.movieapplication.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/haliltprkk/movieapplication/presentation/movie_detail/MovieDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.haliltprkk.movieapplication.presentation.movie_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.haliltprkk.movieapplication.R
+
+class MovieDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
