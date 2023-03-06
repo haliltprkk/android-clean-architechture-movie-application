c5eab8089d7ea02bf6e5c86b1e7347dcd83a4a02
haliltprkk <haliltprkk@gmail.com>
2023-03-06 23:25:45 +0300

    Add scope for some dependencies

--- app/src/main/java/com/haliltprkk/movieapplication/di/UserCaseModule.kt
+++ app/src/main/java/com/haliltprkk/movieapplication/di/UserCaseModule.kt
@@ -10,21 +10,25 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class UserCaseModule {
     @Binds
+    @Singleton
     abstract fun bindGetPopularMoviesUseCase(
         getPopularMoviesUseCaseImpl: GetPopularMoviesUseCaseImpl
     ): GetPopularMoviesUseCase
 
     @Binds
+    @Singleton
     abstract fun bindGetMovieDetailUseCase(
         getMovieDetailUseCaseImpl: GetMovieDetailUseCaseImpl
     ): GetMovieDetailUseCase
 
     @Binds
+    @Singleton
     abstract fun bindSearchMovieUseCase(
         searchMovieUseCaseImpl: SearchMovieUseCaseImpl
     ): SearchMovieUseCase
